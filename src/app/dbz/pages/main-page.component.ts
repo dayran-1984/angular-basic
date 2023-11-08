a2be311f554d52a1da37ee11a4587d815487aaf8
dayran-1984 <dayran1984@hotmail.com>
2023-11-07 22:55:56 -0500

    working with modules

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+  public characters: Character[] = [
+    { name: "Krillin", power:500 },
+    { name: "Goku", power: 10000},
+    { name: "Vegeta", power: 7500},
+  ];
+
+}
